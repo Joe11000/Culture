43f548c5d05c2ff619dc3f3925e35cfef7fe3ac6
Joe <JoeNoonan27@gmail.com>
2014-03-11 23:12:09 -0500

    added possibly redundent non_culture stuff

---
+++ bash_profile
@@ -0,0 +1,17 @@
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+
+# ******JOE_ADDED ********************************
+source ~/coding/dbc/culture/culture/Joe_Settings_Functions
+source ~/coding/dbc/culture/culture/Joe_Settings_Aliases
+
+#source ~/dbc/culture/culture/Joe_Settings_aliases_WIP
+
+PATH="/home/joe/coding/culture/JoeCommands:~/coding/dbc/culture/culture/:$PATH"
+#****************************************************
+
+
+# function subl()
+# {
+# 	sublime-text $@ &
+# 	exit 0
+# }
---
+++ chicago_tribune_reader.js
@@ -0,0 +1,27 @@
+#remove crap preventing me from reading ChicagoTribune.com
+
+
+(function(){
+  var newscript = document.createElement('script');
+     newscript.type = 'text/javascript';
+     newscript.async = true;
+     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+})();
+
+
+$('.dialog-wrapper.flexible-registration-modal').remove()
+
+
+window.setTimeout('partB',4000)
+$('#dialog-overlayed').remove()
+
+$('body').css('overflow', 'scroll')
+         .css('height', screen.height)
+
+
+
+
+# window.$$('#story-body-text').remove
+
+# window.dialog-overlayed
